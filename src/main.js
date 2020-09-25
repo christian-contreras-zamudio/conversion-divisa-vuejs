ac31844c96cdd9e7d4b503cb145338fd80054305
christian <czccontreras@gmail.com>
2020-09-25 11:00:53 -0500

    se agrego moment para fechas

--- src/main.js
+++ src/main.js
@@ -6,6 +6,14 @@ import store from "./store";
 import vuetify from "./plugins/vuetify";
 import "roboto-fontface/css/roboto/roboto-fontface.css";
 import "@mdi/font/css/materialdesignicons.css";
+import VueMoment from "vue-moment";
+import moment from "moment-timezone";
+
+require("moment/locale/es");
+
+Vue.use(VueMoment, {
+  moment
+});
 
 Vue.config.productionTip = false;
 
