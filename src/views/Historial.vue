bb2911f3e44f854702e7e62a30062deaba386e1b
christian <czccontreras@gmail.com>
2020-09-25 11:01:26 -0500

    se rellena tabla con la cosulta al endpoint de historial

--- src/views/Historial.vue
+++ src/views/Historial.vue
@@ -14,114 +14,62 @@
       </v-card-title>
       <v-data-table
         :headers="headers"
-        :items="desserts"
+        :items="historial"
         :search="search"
-      ></v-data-table>
+        :loading="loading"
+        loading-text="Consultando… por favor espere"
+        sort-by="createdAt"
+        :sort-desc="['createdAt']"
+      >
+        <template v-slot:item.createdAt="{ item }">
+          {{
+            item.createdAt | moment("timezone", "America/Mexico_City", "LLLL")
+          }}
+        </template>
+      </v-data-table>
     </v-card>
   </div>
 </template>
 
 <script>
+import { get } from "axios";
 export default {
   data() {
     return {
+      loading: false,
+      error: false,
       search: "",
       headers: [
         {
-          text: "Dessert (100g serving)",
+          text: "From",
           align: "start",
-          sortable: false,
-          value: "name"
+          value: "fromCurrency"
         },
-        { text: "Calories", value: "calories" },
-        { text: "Fat (g)", value: "fat" },
-        { text: "Carbs (g)", value: "carbs" },
-        { text: "Protein (g)", value: "protein" },
-        { text: "Iron (%)", value: "iron" }
+        { text: "To", value: "toCurrency" },
+        { text: "Monto", value: "amount" },
+        { text: "Creado", value: "createdAt" }
       ],
-      desserts: [
-        {
-          name: "Frozen Yogurt",
-          calories: 159,
-          fat: 6.0,
-          carbs: 24,
-          protein: 4.0,
-          iron: "1%"
-        },
-        {
-          name: "Ice cream sandwich",
-          calories: 237,
-          fat: 9.0,
-          carbs: 37,
-          protein: 4.3,
-          iron: "1%"
-        },
-        {
-          name: "Eclair",
-          calories: 262,
-          fat: 16.0,
-          carbs: 23,
-          protein: 6.0,
-          iron: "7%"
-        },
-        {
-          name: "Cupcake",
-          calories: 305,
-          fat: 3.7,
-          carbs: 67,
-          protein: 4.3,
-          iron: "8%"
-        },
-        {
-          name: "Gingerbread",
-          calories: 356,
-          fat: 16.0,
-          carbs: 49,
-          protein: 3.9,
-          iron: "16%"
-        },
-        {
-          name: "Jelly bean",
-          calories: 375,
-          fat: 0.0,
-          carbs: 94,
-          protein: 0.0,
-          iron: "0%"
-        },
-        {
-          name: "Lollipop",
-          calories: 392,
-          fat: 0.2,
-          carbs: 98,
-          protein: 0,
-          iron: "2%"
-        },
-        {
-          name: "Honeycomb",
-          calories: 408,
-          fat: 3.2,
-          carbs: 87,
-          protein: 6.5,
-          iron: "45%"
-        },
-        {
-          name: "Donut",
-          calories: 452,
-          fat: 25.0,
-          carbs: 51,
-          protein: 4.9,
-          iron: "22%"
-        },
-        {
-          name: "KitKat",
-          calories: 518,
-          fat: 26.0,
-          carbs: 65,
-          protein: 7,
-          iron: "6%"
-        }
-      ]
+      historial: []
     };
+  },
+  methods: {
+    consultar() {
+      let vm = this;
+      this.loading = true;
+      get(`${process.env.VUE_APP_HOST}/api/moneda/historial`)
+        .then(function(response) {
+          vm.historial = response.data;
+          vm.loading = false;
+        })
+        .catch(function(error) {
+          vm.loading = false;
+          vm.error = true;
+          vm.mensajeError = error.response.data.message;
+        });
+    }
+  },
+  created() {
+    this.consultar();
   }
 };
 </script>
